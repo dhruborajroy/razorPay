09acaeaf86378b7de231b0bf88550d415731c656
Dhrubo Raj Roy <Dhruborajroy3@gmail.com>
2021-07-12 13:37:11 +0600

    Add files via upload

---
+++ razorPay.php
@@ -0,0 +1,46 @@
+<?php 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.razorpay.com/v1/orders');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"amount\": 10000,\"currency\": \"INR\",\"receipt\": \"1\",\"account_id\": \"acc_Ef7ArAsdU5t0XL\"}");
+curl_setopt($ch, CURLOPT_USERPWD, 'rzp_test_LioWeLFwX8nPhq'.':'.'GyhCxizWH0PtFX7LKIDVmjlu');
+$headers = array();
+$headers[] = 'Content-Type: application/json';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+$result=json_decode($result,true);
+// echo "<pre>";
+// print_r($result);
+?>
+<button id="rzp-button1">Pay</button>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<script>
+var options = {
+    "key": "rzp_test_LioWeLFwX8nPhq", // Enter the Key ID generated from the Dashboard
+    "amount": "10000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Acme Corp",
+    "description": "Test Transaction",
+    "image": "https://example.com/your_logo",
+    "order_id": "<?php echo $result['id']?>",
+    "handler": function (response){
+        // alert(response.razorpay_payment_id);
+        // alert(response.razorpay_order_id);
+        // alert(response.razorpay_signature);
+        document.write(response.razorpay_payment_id+"<br>");
+        document.write(response.razorpay_order_id+"<br>");
+        document.write(response.razorpay_signature+"<br>");
+
+    }
+};
+var rzp1 = new Razorpay(options);
+document.getElementById('rzp-button1').onclick = function(e){
+    rzp1.open();
+    e.preventDefault();
+}
+</script>
